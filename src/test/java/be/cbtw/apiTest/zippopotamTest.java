baac6c261d73b0f4900385f76b41ea0ba3eb37f2
AbdelhakimMAZOUZ <amazouz@cbtw.tech>
2024-07-09 16:30:38 +0200

    adding shcema validator test

--- src/test/java/be/cbtw/apiTest/zippopotamTest.java
+++ src/test/java/be/cbtw/apiTest/zippopotamTest.java
@@ -1,9 +1,8 @@
 package be.cbtw.apiTest;
 
 
-
-
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
@@ -25,6 +24,7 @@ public class zippopotamTest {
                 .assertThat()
                 .statusCode(200);
     }
+
     @Test
     public void validateNumberOfPageForGivenBookTest() {
         given()
@@ -34,7 +34,7 @@ public class zippopotamTest {
                 .then()
                 .assertThat()
                 .statusCode(200)
-                .body("pages",equalTo(234));
+                .body("pages", equalTo(234));
 
     }
 
@@ -42,32 +42,31 @@ public class zippopotamTest {
     public void validateRandomUserUsingGPath() {
         given()
                 .when()
-                .get("https://jsonplaceholder.typicode.com/posts/1")
+                .get("http://localhost:3000/posts/1")
                 .then()
-                .assertThat()
-                .body("userId",equalTo(1))
-                .body("user.address.geo.lat",equalTo("-37.3159"))
-                .body("user.address.geo.log",equalTo("-81.1496"));
-
-
+                .assertThat(
+                ).body("id", equalTo("1"))
+                .body("title", equalTo("Mock Post 1"))
+                .body("author.name", equalTo("John Doe"))
+                .body("author.address.city", equalTo("Springfield"))
+                .body("author.company.name", equalTo("ACME Inc."))
+                .body("comments.size()", equalTo(2))
+                .body("comments[0].body", equalTo("This is comment 1"))
+                .body("comments[1].user.name", equalTo("Michael Johnson"));
     }
 
-
-
-
-
-        @Test
-        public void serviceWithAuthenticationTest() {
-          given()
-                 .auth()
-                  .basic("postman", "password")
-                  .when()
-                  .get("https://postman-echo.com/basic-auth")
-                  .then()
-                  .assertThat()
-                  .body("authenticated", equalTo(true))
-                  .statusCode(200);
-        }
+    @Test
+    public void serviceWithAuthenticationTest() {
+        given()
+                .auth()
+                .basic("postman", "password")
+                .when()
+                .get("https://postman-echo.com/basic-auth")
+                .then()
+                .assertThat()
+                .body("authenticated", equalTo(true))
+                .statusCode(200);
+    }
 
     @Test
     public void serviceWithAuthenticationUsingPreemptiveTest() {
@@ -83,29 +82,36 @@ public class zippopotamTest {
                 .statusCode(200);
     }
 
+// need to have key github account
+//    @Test
+//    public void testOAuth2Authentication() {
+//        // Replace with your actual GitHub OAuth token
+//        String accessToken = "your_github_oauth_token";
+//
+//        RestAssured.baseURI = "https://api.github.com";
+//
+//        given()
+//                .auth()
+//                .oauth2(accessToken)
+//                .when()
+//                .get("/user")
+//                .then()
+//                .statusCode(200)
+//                .body("login", equalTo("your_github_username")); // Replace with your GitHub username
+//    }
 
-        @Test
-        public void testOAuth2Authentication() {
-            // Replace with your actual GitHub OAuth token
-            String accessToken = "your_github_oauth_token";
+    @Test
+    public void JsonSchemaValidationTest() {
+        given()
+                .when()
 
-            RestAssured.baseURI = "https://api.github.com";
+                .get("https://bookstore.toolsqa.com/BookStore/v1/Books")
+                .then()
+                .assertThat()
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("be.cbtw.apiTest.schema/list-book-schema-validator.json"))
+                .log();
 
-            given()
-                    .auth()
-                    .oauth2(accessToken)
-                    .when()
-                    .get("/user")
-                    .then()
-                    .statusCode(200)
-                    .body("login", equalTo("your_github_username")); // Replace with your GitHub username
-        }
-
-
-
-
-
-
+    }
 
 
 }
